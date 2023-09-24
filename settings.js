e2d3bcac162d3a3a6897b4a521bf647e185378c8
Rasinduharsita <132072515+Rasinduharsita@users.noreply.github.com>
2023-09-24 05:15:34 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ const chalk = require('chalk')
 */
 global.OFFLINE = false // if you want put bot offline put true
 global.BOT_PRIVATE = true // if you want use bot privet 
-global.AUTO_CHATGPT = false // if you want auto ai bot mod
-global.READ_MESSAGE = false // if you want read message from bot
+global.AUTO_CHATGPT = true // if you want auto ai bot mod
+global.READ_MESSAGE = true // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
 
@@ -17,11 +17,11 @@ global.ONLY_GROUPS = false // Only Groups Use
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OWNER_NAME = 'R.H.GHOST' // Owner Name
-global.OWNER_NUMBER = ['94754124943'] //Owner Number
+global.OWNER_NUMBER = ['94770611055'] //Owner Number
 global.BOT_ADMINS = ['94754124943','94743271170'] //Bot admins numbers Like a Sudo Numbers
-global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
-global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
+global.BLOCK_CHAT = [xxx,sex] // If you want Block Bot From Chats Use .jid Command and put Jids
+global.IMAGE_QUALITY = 'high' // Available Quality Are => low | mediam | high
+global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) R.H.GHOST 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
@@ -39,8 +39,8 @@ global.IMAGE_CMD = 'aiimg' // .aiimg Command
 global.PORT = process.env.PORT || '5000' // Port No need Change.
 
 mess = {
-	owner: '*This Command is only For Owner.*',
-	need_text: '*Please Give Some words*'
+	owner: '*This Command is only For Owner ඔයා මගේ ස්වාමියා නොවේ.....*',
+	need_text: '*Please Give Some words  මට අවශය වාක්‍ය ලබා දෙන්න....*'
 }
 react = {
     ai_message : '🤖',

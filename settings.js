1182b724a26e05054baeb8797f9c0ffc8f134ee1
Rasinduharsita <132072515+Rasinduharsita@users.noreply.github.com>
2023-09-28 03:44:01 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVATE = true // if you want use bot privet 
+global.BOT_PRIVATE = false // if you want use bot privet 
 global.AUTO_CHATGPT = true // if you want auto ai bot mod
 global.READ_MESSAGE = true // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
@@ -19,7 +19,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 global.OWNER_NAME = 'R.H.GHOST' // Owner Name
 global.OWNER_NUMBER = ['94770611055'] //Owner Number
 global.BOT_ADMINS = ['94754124943','94743271170'] //Bot admins numbers Like a Sudo Numbers
-global.BLOCK_CHAT = [xxx,sex] // If you want Block Bot From Chats Use .jid Command and put Jids
+global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'high' // Available Quality Are => low | mediam | high
 global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) R.H.GHOST 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
 
